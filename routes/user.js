a842d402e33b1fd916826de1bc190d41e7056118
ShahebazKhanPathan <pathanshahebazk@gmail.com>
2024-07-09 12:45:25 +0530

    Added middlewares for router, json data, and error handling

--- routes/user.js
+++ routes/user.js
@@ -2,5 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post("/", (req, res) => {
-    res.send(req.body);
+    const data = req.body;
+    return res.status(200).send(data);
 });
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,10 @@ const app = express();
 const user = require('../routes/user');
 
 module.exports = (app) => {
-    app.use("/user", user);
+    app.use(express.json());
+    app.use("/api/user", user);
+    app.use((err, req, res, next) => {
+        res.status(500).send("Something went wrong.");
+    });
 }
 
