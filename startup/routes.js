a76ee57f6a4273c93fae396c0a6d5baa7209c18f
ShahebazKhanPathan <pathanshahebazk@gmail.com>
2024-07-09 12:50:49 +0530

    Added cors middleware

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const user = require('../routes/user');
+const cors = require("cors");
 
 module.exports = (app) => {
+    app.use(cors());
     app.use(express.json());
     app.use("/api/user", user);
     app.use((err, req, res, next) => {
